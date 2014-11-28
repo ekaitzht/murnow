5fcd14b93e4c5a3dba42e5e6d61e91e6021fb865
Ekaitz <ekaitz.hernandez@gmail.com>
2014-11-28 17:36:15 +0100

    angular app all in application layout

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  root to: 'application#angular'
 
   # Example resource route with options:
   #   resources :products do
