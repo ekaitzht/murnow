d96518e0ef6e53bd2e790d011ab2b666465912cc
Ekaitz <ekaitz.hernandez@gmail.com>
2014-12-06 20:47:17 +0100

    image_url from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206193428) do
+ActiveRecord::Schema.define(version: 20141206194606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20141206193428) do
     t.decimal  "sku_currency"
     t.decimal  "product_stars",            default: 0.0
     t.integer  "number_reviews",           default: 0
-    t.string   "image_url"
+    t.text     "image_url"
     t.string   "hash_url_image"
     t.text     "long_description"
   end
