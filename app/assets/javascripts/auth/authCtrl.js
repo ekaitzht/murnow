951f3c786f25ee64d19243c407224c30aa7e486b
Ekaitz <ekaitz.hernandez@gmail.com>
2014-12-09 15:59:41 +0100

    Feedback user wrong password or email.

--- app/assets/javascripts/auth/authCtrl.js
+++ app/assets/javascripts/auth/authCtrl.js
@@ -8,6 +8,14 @@ function($scope, $state, Auth){
   $scope.login = function() {
     Auth.login($scope.user).then(function(){
       $state.go('home');
+    }).then(function(response) {
+        // Successfully recovered from unauthorized error.
+        // Resolve the original request's promise.
+
+    }, function(error) {
+        // There was an error logging in.
+        // Reject the original request's promise.
+        $scope.login.errorMessage = error.data.error;
     });
   };
 
