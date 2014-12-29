b0598400ee36ec03a11afbd2d03e7541a4017d83
Ekaitz <ekaitz.hernandez@gmail.com>
2014-12-29 16:35:18 +0100

    Updated angul-material

--- vendor/assets/bower_components/angular-material/modules/js/radioButton/radioButton.js
+++ vendor/assets/bower_components/angular-material/modules/js/radioButton/radioButton.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.6.1
+ * v0.7.0-rc1-master-84842ff
  */
 (function() {
 'use strict';
@@ -67,10 +67,8 @@ function mdRadioGroupDirective($mdUtil, $mdConstant, $mdTheming) {
 
   function linkRadioGroup(scope, element, attr, ctrls) {
     $mdTheming(element);
-    var rgCtrl = ctrls[0],
-      ngModelCtrl = ctrls[1] || {
-        $setViewValue: angular.noop
-      };
+    var rgCtrl = ctrls[0];
+    var ngModelCtrl = ctrls[1] || $mdUtil.fakeNgModel();
 
     function keydownListener(ev) {
       if (ev.keyCode === $mdConstant.KEY_CODE.LEFT_ARROW || ev.keyCode === $mdConstant.KEY_CODE.UP_ARROW) {
@@ -149,10 +147,16 @@ function mdRadioGroupDirective($mdUtil, $mdConstant, $mdTheming) {
     );
 
     if (buttons.count()) {
+      var validate = function (button) {
+        // If disabled, then NOT valid
+        return !angular.element(button).attr("disabled");
+      };
       var selected = parent[0].querySelector('md-radio-button.md-checked');
-      var target = buttons[increment < 0 ? 'previous' : 'next'](selected) || buttons.first();
+      var target = buttons[increment < 0 ? 'previous' : 'next'](selected, validate) || buttons.first();
       // Activate radioButton's click listener (triggerHandler won't create a real click event)
       angular.element(target).triggerHandler('click');
+
+
     }
   }
 
