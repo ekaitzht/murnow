44f434d456cbb7d4c66d3628efc44b374119d9a7
Ekaitz <ekaitz.hernandez@gmail.com>
2014-12-29 11:31:32 +0100

    Implemented facebook-omniauth bbackend.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,17 @@ class User < ActiveRecord::Base
   	# Include default devise modules. Others available are:
   	# :lockable, :timeoutable and :omniauthable
   	devise :database_authenticatable, :confirmable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
-  	validates :username, presence: true
+  	validates :username, presence: true 
 
+	def self.from_omniauth(auth)
+	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	    user.email = auth.info.email
+	    user.password = Devise.friendly_token[0,20]
+	    user.username = auth.info.name   # assuming the user model has a name
 
+	  end
+	end
 end
