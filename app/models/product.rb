1e12bb1f89310d1ac233c90432dcbc9b9bb1f4ca
Ekaitz <ekaitz.hernandez@gmail.com>
2015-01-05 18:29:37 +0100

    Added limit size 2000 result in elasticsearch.

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,7 +19,8 @@ class Product < ActiveRecord::Base
             # Here you can add what search field can be matcheables
             fields: ['product_name^10', 'long_product_description'] 
           }
-        }
+        },
+        from: 0, size: 1000
       }
     )
   end
