ec9037ee86983b6f6cfdcd79696c636ade182bbd
Ekaitz <ekaitz.hernandez@gmail.com>
2015-01-09 13:34:37 +0100

    Updating profile.html when you save in _edit_profile.html.

--- app/assets/javascripts/profile/profile.js
+++ app/assets/javascripts/profile/profile.js
@@ -1,13 +1,19 @@
 angular.module('murnow')
 .controller('Profile', [
-'$scope','User',
-function($scope, User){
+'$scope','User','$state', 'Auth',
+function($scope, User, $state, Auth){
  $scope.user = User.user_session;
 
   $scope.updateUser = function() {
     
-    User.updateUser($scope.user);
+    User.updateUser($scope.user)
+    
+    Auth.currentUser().then(function(user) {
+       	 User.setUser(user);
+         $state.go('profile');
+    }, function(error) {
+        // unauthenticated error
+    });
   };
 
 }]);
-
