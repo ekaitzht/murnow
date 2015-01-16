a2df207b6f22c04d66b0417906832a1fa6774006
Ekaitz <ekaitz.hernandez@gmail.com>
2015-01-16 17:53:44 +0100

    Adddes unique to skin_problems.name

---
+++ db/migrate/20150116165225_add_unique_name_to_skin_problems.rb
@@ -0,0 +1,7 @@
+class AddUniqueNameToSkinProblems < ActiveRecord::Migration
+  def change
+  	add_index :skin_problems, :name, :unique => true
+
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115115013) do
+ActiveRecord::Schema.define(version: 20150116165225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20150115115013) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "skin_problems", ["name"], name: "index_skin_problems_on_name", unique: true, using: :btree
 
   create_table "user_skin_problems", force: true do |t|
     t.integer "user_id"
