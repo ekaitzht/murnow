050317dcb38f608c83b2672cfcf8ce566228f98c
Ekaitz <ekaitz.hernandez@gmail.com>
2015-01-21 00:12:08 +0100

    Changed parameters to addReview to fullfill strong parameters in the backend.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
   
   def create
     @product = Product.find(params[:product_id])
+    
     review = @product.reviews.create(review_params.merge(user_id: current_user.id))
     if (review.repurchase == true) then
       @product.increment!(:buyers)
