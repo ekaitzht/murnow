ad4b1e177bc3efb22a03225486dbe7d461728434
Ekaitz <ekaitz.hernandez@gmail.com>
2015-02-13 12:11:10 +0100

    Adding ENV STAGING_ACTIVE to control AUTH in staging deployment.

--- config/initializers/staging_auth.rb
+++ config/initializers/staging_auth.rb
@@ -1,10 +1,13 @@
 MurnowAngular::Application.configure do
     # Restrict access with HTTP Basic Auth for staging environments
-    unless ENV['STAGING_AUTH'].blank?
-        config.middleware.use '::Rack::Auth::Basic' do |username, password|
-            ENV['STAGING_AUTH'].split(';').any? do |pair|
-                [username, password] == pair.split(':')
-            end
-        end
-    end
+    
+    if  ENV['STAGING_ACTIVE']
+		unless ENV['STAGING_AUTH'].blank?
+	        config.middleware.use '::Rack::Auth::Basic' do |username, password|
+	            ENV['STAGING_AUTH'].split(';').any? do |pair|
+	                [username, password] == pair.split(':')
+	            end
+	        end
+	    end
+	end
 end
