11d5dcf5a249932933576976a25fcf0ab9908b7e
Ekaitz <ekaitz.hernandez@gmail.com>
2015-02-13 16:53:21 +0100

    Applied cdn] distribution for images_products.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
+  #config.static_cache_control = 'public, max-age=31536000'
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
