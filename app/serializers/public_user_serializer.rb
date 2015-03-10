c80a4e17025aeb6206d32c77ec6c1c3c967afcec
Ekaitz <ekaitz.hernandez@gmail.com>
2015-03-10 09:59:35 +0100

    Addes only glyphicon library and username in serializer

--- app/serializers/public_user_serializer.rb
+++ app/serializers/public_user_serializer.rb
@@ -1,6 +1,6 @@
 class PublicUserSerializer < ActiveModel::Serializer
 	self.root = false
-	  attributes  :id,:age, :bio, :favourite_brand, :image, :skin_color, :skin_tone, :skin_type
+	  attributes  :id,:age, :bio, :favourite_brand, :image, :skin_color, :skin_tone, :skin_type, :username
 	  
 	  has_many :reviews,  serializer: UserProfileReviewsSerializer
 end
