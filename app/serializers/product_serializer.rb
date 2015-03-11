cbbf977d976b87ea9fb7265f69b180dec191a89b
Ekaitz <ekaitz.hernandez@gmail.com>
2015-03-11 18:41:41 +0100

    Added brand_name

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-	  attributes :id, :product_name, :upvotes, :product_id, :hash_url_image, :buyers, :not_buyers, :product_stars, :brand_name
+	  attributes :id, :product_name, :upvotes, :product_id, :hash_url_image, :buyers, :not_buyers, :product_stars, :brand_name, :category
 	  
 	  has_many :reviews
 end
