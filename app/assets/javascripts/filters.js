5f0734924137cd4e6cdab66551d4c25dbaab070f
Ekaitz <ekaitz.hernandez@gmail.com>
2015-03-17 12:36:33 +0100

    Added moment().twitterLong() and fromNow filter to public profile.

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,15 +1,7 @@
 angular.module('murnowFilters',[]).
   filter('fromNow',  function() {
     return function(dateString) {
-	  /*if(moment(dateString).hour() < 24 ) {
-		return moment(dateString).fromNow()
-	  } else if (moment(dateString).hour() < 365){
-		return moment(dateString).format("MMM DD");
-	  } else {
-		return moment(dateString).format("MMM DD YYYY");
-	  }*/
-	  return moment(dateString).twitter();
-      
+	  return moment(dateString).twitterLong();
     };
   });
   
