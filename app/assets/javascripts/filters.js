cd01e5f5582dc62ba29a5b7f737e294c02fb437d
Ekaitz <ekaitz.hernandez@gmail.com>
2015-08-11 13:19:06 +0200

    Applied filter for limitLength and we control in profile the limit length depends if we are the window is less than 900px

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -3,6 +3,28 @@ angular.module('murnowFilters',[]).
     return function(dateString) {
 	  return moment(dateString).twitterLong();
     };
+  }).
+   filter('limitLength',  function() {
+    return function(overString, numChar, rootScope) {
+	    
+	    if( rootScope !== undefined) {
+		    var state = rootScope.getCurrentNameState();
+		    
+		    if( state == 'profile' && (window.innerWidth > 900)){ 
+			    return overString;
+		    }
+	    }
+	    
+	    if( overString.length >= numChar ) {
+			return overString.substring(0, numChar) + '...';
+				
+		} else {
+			return overString;
+		}
+    };
   });
+
+  
+  
   
   
