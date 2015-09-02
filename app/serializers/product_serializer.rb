7dde2313f478d5381068b459dab481b55e9d5c79
Ekaitz <ekaitz.hernandez@gmail.com>
2015-09-02 15:38:39 +0200

    fixed some issues related with schema.org

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-	  attributes :id, :product_name, :upvotes, :hash_url_image, :buyers, :not_buyers, :product_stars, :brand_name, :category, :rating
+	  attributes :id, :product_name, :upvotes, :hash_url_image, :buyers, :not_buyers, :product_stars, :brand_name, :category, :rating,:number_reviews
 	  
 	  has_many :reviews
 end
