338d38d79964af20e0bf267aab0c091f21c29149
Ekaitz <ekaitz.hernandez@gmail.com>
2015-09-23 18:58:41 +0200

    Fixed problem with fixed  scroll

--- app/assets/javascripts/directives/myPostRepeat.js
+++ app/assets/javascripts/directives/myPostRepeat.js
@@ -1,8 +1,9 @@
 angular.module('myPostRepeat', [])
 .directive('myPostRepeat',  ['$rootScope','$location',function($rootScope, $location) {
   return function(scope, element, attrs) {
-		  			  	
+		if( ($rootScope.fromState === '' && $rootScope.destState === 'list_products') || ($rootScope.fromState === 'products' && $rootScope.destState === 'list_products')) {	
 		  $("md-content").scrollTop($rootScope.scrollProductsPosition);
 		  //$location.hash($rootScope.idProductClicked);
+		}  	
   };
 }]);
