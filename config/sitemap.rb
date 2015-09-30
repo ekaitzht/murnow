5d537d14a839a67c548858170d1505a0abba3e12
Ekaitz <ekaitz.hernandez@gmail.com>
2015-09-30 17:22:07 +0200

    Added sitemap with friendly_id

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,8 +3,9 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'http://www.murnow.com'
 SitemapGenerator::Sitemap.create do
-  (29202..33328).each do |i|
-  	add '/products/'+i.to_s, :changefreq => 'weekly' 
+
+  Product.find_each do |product|
+    add '/products/'+product.slug, :lastmod => product.updated_at, :priority => product.rating / 100
   end
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
