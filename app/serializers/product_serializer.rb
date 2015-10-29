d725386c84097cb5985cd68e502c4f2a85a05610
Ekaitz <ekaitz.hernandez@gmail.com>
2015-10-29 21:26:41 +0100

    Added :original_url in product_serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-	  attributes :id, :product_name, :upvotes, :hash_url_image, :buyers, :not_buyers, :product_stars, :brand_name, :category, :rating,:number_reviews
+	  attributes :id, :product_name, :upvotes, :hash_url_image, :buyers, :not_buyers, :product_stars, :brand_name, :category, :rating,:number_reviews, :original_url
 	  
 	  has_many :reviews
 end
