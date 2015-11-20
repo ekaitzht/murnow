9511b2f878effdd7481764ac6c55733339fa31a2
Ekaitz <ekaitz.hernandez@gmail.com>
2015-11-20 12:33:22 +0100

    Started follow system backend.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,16 @@ class User < ActiveRecord::Base
   	has_many :user_skin_problems
 	has_many :skin_problems, :through => :user_skin_problems
 	has_many :votes
-	has_many :reviews
+	has_many :reviews, dependent: :destroy
 	
-	has_many :sent_invitations, :class_name => 'Invitation', :foreign_key => 'sender_id'
+	has_many :active_relationships, class_name:  "Relationship",foreign_key: "follower_id",dependent:   :destroy
+	has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
+
+    has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
+
+	
+	#has_many :sent_invitations, :class_name => 'Invitation', :foreign_key => 'sender_id'
 	belongs_to :invitation
 	
   	devise :database_authenticatable, :confirmable, :registerable,
@@ -86,6 +93,23 @@ class User < ActiveRecord::Base
         		user.email = data["email"] if user.email.blank?
       		end
     	end
+  	end
+  	
+  	
+	 # Follows a user.
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+	
+	# Unfollows a user.
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+	
+	
+	# Returns true if the current user is following the other user.
+	def following?(other_user)
+	    following.include?(other_user)
   	end
   	
   	private
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should follow and unfollow a user" do
+    ekaitz  = users(:ekaitz)
+    ulaize   = users(:ulaize)
+    assert_not ekaitz.following?(ulaize)
+    ekaitz.follow(ulaize)
+    assert ekaitz.following?(ulaize)
+    assert ulaize.followers.include?(ekaitz)
+    ekaitz.unfollow(ulaize)
+    assert_not ekaitz.following?(ulaize)
+  end
 end
+
+
