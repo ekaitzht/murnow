9e0cd523117649d61c2ef5b6ee7b429f326da1cf
Ekaitz <ekaitz.hernandez@gmail.com>
2015-11-20 16:43:54 +0100

    Added count followers in PublicUserSerializer

--- app/serializers/public_user_serializer.rb
+++ app/serializers/public_user_serializer.rb
@@ -1,13 +1,18 @@
 class PublicUserSerializer < ActiveModel::Serializer
 	self.root = false
-	attributes  :id,:age,:eye_color, :bio, :favourite_brand, :hash_url_image, :skin_tone, :skin_type, :username,:following
+	attributes  :id,:age,:eye_color, :bio, :favourite_brand, :hash_url_image, :skin_tone, :skin_type, :username,:following, :followers
 	  
 	  
 	def following
 	    collection = object.active_relationships.to_a
-	    { count: collection_length }
+	    return collection.length 
 	end
 	
+	
+	def followers
+	    collection = object.passive_relationships.to_a
+	    return collection.length 
+	end
 	
 end
 
